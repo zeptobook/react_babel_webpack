0b51556da9888f76bffad310fe8b8f3821a10645
ZeptoBook <44328960+zeptobook@users.noreply.github.com>
2019-09-27 09:38:14 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Creating your React app with Webpack and Babel - ZeptoBook</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
